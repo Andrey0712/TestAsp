96b50747722acb9fbc8f3f51b2051ec3b94d4264
nashemistoart@gmail.com <nashemistoart@gmail.com>
2021-12-22 19:30:37 +0200

    add controler

---
+++ TestAsp/Controlers/ProductController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using TestAsp.Data;
+using TestAsp.Model;
+
+namespace TestAsp.Controlers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+        private EFContext _context { get; set; }
+        public ProductController(EFContext context)
+        {
+            _context = context;
+        }
+        [HttpGet]
+        public IActionResult GetPage([FromQuery] GetPageModel model)
+        {
+            int count = 2;
+            int skip = model.Page != 0 ? (model.Page - 1) * count : 0;
+           
+            string search = model.Search != null ? model.Search : "";
+
+            var returnProducts = _context.Products.Where(x => x.Name.Contains(search) || x.Description.Contains(search));
+            return Ok(new
+            {
+                data = new
+                {
+                    data = returnProducts.Select(x => x).Skip(skip).Take(count).ToList(),
+                    current_page = model.Page,
+                    last_page = (Math.Ceiling((decimal)returnProducts.Count() / count)),
+                    total = returnProducts.Count(),
+                    per_page = count
+                },
+                search = model.Search
+            });
+        }
+    }
+}
